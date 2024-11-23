79cae07a90dbbd636021f647b7849bb7c76d5fcf
chetvertka <78110087+GGgg2005-art@users.noreply.github.com>
2024-11-23 18:39:25 +0200

    Delete js directory

--- js/burger.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const burgerMenu = document.querySelector('.burger-menu');
-    const navMenuList = document.querySelector('.nav-menu__list');
-
-    burgerMenu.addEventListener('click', () => {
-        navMenuList.classList.toggle('active');
-        burgerMenu.classList.toggle('open'); // Додаємо/знімаємо клас .open для анімації
-    });
-});
